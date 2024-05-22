f2a2710f53f5ebc7504eec54b949883eddd448ad
Janiec Dejmian <janiec.damian04@gmail.com>
2024-05-22 22:26:04 +0200

    lol :)

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,5 +1,10 @@
-﻿
-public class Samochod
+﻿class Program
 {
-
+    static void Main()
+    {
+        Samochod s = new Samochod();
+        s.Marka = "nigga car";
+        Console.WriteLine(s.Marka);
+        Console.WriteLine(Samochod.ilosc_samochodow);
+    }
 }
---
+++ Lab01/Samochod.cs
@@ -0,0 +1,46 @@
+﻿public class Samochod
+{
+
+    private string marka;
+    private string model;
+    private int iloscDrzwi;
+    private int pojemnoscSilnika;
+    private double srednieSpalanie; //zmienne
+
+    public static int ilosc_samochodow = 0;
+
+
+    public string Marka //funkcje, metody - jak zwal, tak ruchal wojtka pies
+    {
+        get { return marka; }
+        set { marka = value; }
+    }
+    public string Model
+    {  
+        get { return model; } 
+        set { model = value; } 
+    }
+    public int IloscDrzwi
+    {
+        get { return iloscDrzwi; }
+        set { iloscDrzwi = value; }
+    }
+    public int PojemnoscSilnika
+    {
+        get { return pojemnoscSilnika; }
+        set { pojemnoscSilnika = value; }
+    }
+    public double SrednieSpalanie
+    {
+        get { return srednieSpalanie; }
+        set { srednieSpalanie = value;}
+    }
+    public Samochod()
+    {
+        ilosc_samochodow += 1;
+    }
+    public Samochod(string _marka,string _model,int _iloscDrzwi,int _pojemnoscSilnika,double _srednieSpalanie)
+    {
+        ilosc_samochodow += 1;
+    }
+}

81675576ff82a0c3604ca1b3d00f2a7e4f1a2c3c
Janiec Dejmian <janiec.damian04@gmail.com>
2024-05-23 09:00:39 +0200

    czemu "Wprowadź komunikata" jest wymagany xddd?

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -2,9 +2,26 @@
 {
     static void Main()
     {
+        /*
         Samochod s = new Samochod();
         s.Marka = "nigga car";
         Console.WriteLine(s.Marka);
-        Console.WriteLine(Samochod.ilosc_samochodow);
+        Console.WriteLine(Samochod.ilosc_samochodow); //*/
+        Samochod s1 = new Samochod();
+        s1.WypiszInfo(); //wypisuje pusty booo.... nie danych przeciez xddd
+        
+        s1.Marka = "Fiat";
+        s1.Model = "126p";
+        s1.IloscDrzwi = 2;
+        s1.PojemnoscSilnika = 650;
+        s1.SrednieSpalanie = 6.0;
+        s1.WypiszInfo();
+
+
+        Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+        s2.WypiszInfo();
+        double kosztPrzejazdu = s2.CenaPaliwa(30.5, 4.85);
+        Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
+        Samochod.WypiszIloscSamochodow();
     }
 }
--- Lab01/Samochod.cs
+++ Lab01/Samochod.cs
@@ -41,6 +41,41 @@
     }
     public Samochod(string _marka,string _model,int _iloscDrzwi,int _pojemnoscSilnika,double _srednieSpalanie)
     {
+        marka = _marka;
+        model = _model;
+        ilosc_samochodow = _iloscDrzwi;
+        pojemnoscSilnika = _pojemnoscSilnika;
+        srednieSpalanie = _srednieSpalanie;
+        
+        
         ilosc_samochodow += 1;
+    }
+    public double ObliczSpalanie(double trasa) 
+        //zakladamy ze uzytkownik nie jest debilem, i nie robi mi pod gorke - trase podaje w kilometrach
+    {
+        double spalanieNaKm = srednieSpalanie / 100; //spalanie na 1km
+        double spalanieLaczne = spalanieNaKm * trasa;
+        
+        return spalanieLaczne;
+    }
+    public double CenaPaliwa(double trasa,double cena)
+    {
+        double spalanie = ObliczSpalanie(trasa);
+        double suma = spalanie * cena;
+        return suma;
+    }
+    public void WypiszInfo()
+    {
+        Console.WriteLine($"Samochod numer: {ilosc_samochodow}");
+        Console.WriteLine($"Marka: {marka}");
+        Console.WriteLine($"Model: {model}");
+        Console.WriteLine($"Ilość Drzwi: {iloscDrzwi}");
+        Console.WriteLine($"Pojemność Silnika: {pojemnoscSilnika}");
+        Console.WriteLine($"Średnie Spalanie: {SrednieSpalanie}");
+        Console.WriteLine("\n\n");
+    }
+    public static void WypiszIloscSamochodow()
+    {
+        Console.WriteLine($"W bazie danych jest {ilosc_samochodow} samochody/ów");
     }
 }
